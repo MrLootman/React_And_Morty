24c88afd6e88d8ea6faa3969c1e50e5fe2d97f04
NICOLAS <nicolas_m_44@hotmail.fr>
2023-09-13 10:40:37 +0200

    Mode Hardcore: ✅

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,7 @@
-import Router from "./components/navigation/Router"
-import NavBar from "./components/globals/NavBar"
-
 function App() {
   return (
     <div className="app">
-      <NavBar />
-      <Router />
+      {/* Les composants enfants de App seront rendus ici */}
     </div>
   )
 }
